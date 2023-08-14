517e9a53bcf5a447829145bd7efd3da206e483c0
ulalawell <roman2000.usik@gmail.com>
2023-08-14 18:27:50 +0300

    add bubble sort

---
+++ bubble_sort.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var data = []int{5, 3, 6, 1}
+
+	fmt.Println(bubbleSort(data))
+}
+
+func bubbleSort(data []int) []int {
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < len(data); j++ {
+			if data[j] > data[i] {
+				data[i], data[j] = data[j], data[i]
+			}
+		}
+	}
+	return data
+}

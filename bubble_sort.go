8e395cc872c9c75bba5523c8fd15ba092234ba24
ulalawell <roman2000.usik@gmail.com>
2023-08-14 19:45:00 +0300

    fix bubble

--- bubble_sort.go
+++ bubble_sort.go
@@ -9,10 +9,10 @@ func main() {
 }
 
 func bubbleSort(data []int) []int {
-	for i := 0; i < len(data); i++ {
-		for j := i; j < len(data); j++ {
-			if data[i] > data[j] {
-				data[i], data[j] = data[j], data[i]
+	for i := 0; i < len(data)-1; i++ {
+		for j := 0; j < len(data)-i-1; j++ {
+			if data[j] > data[j+1] {
+				data[j], data[j+1] = data[j+1], data[j]
 			}
 		}
 	}

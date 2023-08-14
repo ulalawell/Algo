f5f21b45b0a5d9562bdc2c0bf8569137eae9d14c
ulalawell <roman2000.usik@gmail.com>
2023-08-14 19:13:41 +0300

    add insert

--- insert_sort.go
+++ insert_sort.go
@@ -5,13 +5,13 @@ import "fmt"
 func main() {
 	var data = []int{5, 3, 6, 1}
 
-	fmt.Println(bubbleSort(data))
+	fmt.Println(insertSort(data))
 }
 
-func bubbleSort(data []int) []int {
+func insertSort(data []int) []int {
 	for i := 0; i < len(data); i++ {
-		for j := i; j < len(data); j++ {
-			if data[i] > data[j] {
+		for j := 0; j < i; j++ {
+			if data[i] < data[j] {
 				data[i], data[j] = data[j], data[i]
 			}
 		}
